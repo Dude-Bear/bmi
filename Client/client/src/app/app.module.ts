b799cc353f46a0a9cacf5f55f45eaa4953e0a0f8
esc <elias.scharlach@icloud.com>
2021-10-08 15:48:27 +0200

    added a basic button and simple calculator-form

--- Client/client/src/app/app.module.ts
+++ Client/client/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './pages/main/main.component';
 import { CalculatorComponent } from './pages/calculator/calculator.component';
+import { ButtonComponent } from './controls/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     CalculatorComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Client/client/src/app/pages/calculator/calculator.component.ts
+++ Client/client/src/app/pages/calculator/calculator.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+  styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent implements OnInit {
+  title: string = 'BMI-Calculator';
+  result: string = '';
+  isKnownUser: boolean;
+  
+  weight?:number =40;
+  height?:number =187;
+  birthDay: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.isKnownUser = true;
   }
 
+  ngOnInit(): void {}
+
+  CalculateBMI() {
+    console.log(this.weight);
+    console.log(this.height);
+    this.result = String((this.weight?? 0) + (this.height?? 0));
+  }
 }
