9fc3d2c268ccf398265bc1019ae8970428ef855a
Bjoern <bjoern.schriewer@gmail.com>
2021-10-15 14:19:28 +0200

    Server: add PostMapping decorator

--- Server/src/main/java/com/swp/bmi/registration/RegistrationController.java
+++ Server/src/main/java/com/swp/bmi/registration/RegistrationController.java
@@ -1,9 +1,7 @@
 package com.swp.bmi.registration;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/registration")
@@ -12,7 +10,8 @@ public class RegistrationController {
 
     private RegistrationService registrationService;
 
-    public String register(@RequestBody RegistrationRequest request){
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
 }
